6fcd86596d87e603bb1f9409a9a444e3dcd6bb91
FilipFelipe <filipfelipe@outlook.com>
2022-08-31 22:11:00 -0300

    4 - Criando, deletando e editando com Gorm

--- routes/routes.go
+++ routes/routes.go
@@ -14,6 +14,9 @@ func HandleResquest() {
 	Port := os.Getenv("PORT")
 	r.HandleFunc("/", controllers.Home)
 	r.HandleFunc("/api/personalidades", controllers.TodasPersonalidades).Methods("Get")
+	r.HandleFunc("/api/personalidades", controllers.CriaUmaNovaPersonalidade).Methods("Post")
+	r.HandleFunc("/api/personalidades/{id}", controllers.DeletaUmaPersonalidade).Methods("Delete")
 	r.HandleFunc("/api/personalidades/{id}", controllers.RetornaUmaPersonalidade).Methods("Get")
+	r.HandleFunc("/api/personalidades/{id}", controllers.EditaPersonalidade).Methods("Put")
 	log.Fatal(http.ListenAndServe(":"+Port, r))
 }
